6f261ed14af2429019e228c8d219ddf9d2ac523d
Antonio Parraga Navarro <antonio@parraga.es>
2013-03-13 11:07:41 +0100

    Try to make works the phing on travis env

--- build.sh
+++ build.sh
@@ -2,4 +2,4 @@ export PHP_COMMAND=/usr/bin/php
 export PHING_HOME=./build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
 
-${PHING_HOME}/bin/phing -logger phing.listener.DefaultLogger
+cd ${PHING_HOME}/bin/ && phing -logger phing.listener.DefaultLogger
