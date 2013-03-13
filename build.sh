e8ebb8b442608caab8e69fa5785102a88350fca6
Antonio Parraga Navarro <antonio@parraga.es>
2013-03-13 11:20:52 +0100

    Try to use the right env variable to access to the right path (or what)

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 export PHP_COMMAND=/usr/bin/php
-export PHING_HOME=./build-libs/phing-2.4.9
+export PHING_HOME=${TRAVIS_BUILD_DIR}/build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
 
 cd ${PHING_HOME}/bin/ && phing -logger phing.listener.DefaultLogger
