67f2de876d6dba456627317898b3387505479cd1
Antonio Parraga Navarro <antonio@parraga.es>
2013-03-14 17:11:46 +0100

    Remove the php command to search on /usr/bin/php (which is legacy from my previous build box)

--- build.sh
+++ build.sh
@@ -1,7 +1,3 @@
-DIRFILE="dirfile.txt"
-find . -type f > $DIRFILE
-cat $DIRFILE
-export PHP_COMMAND=/usr/bin/php
 export PHING_HOME=${TRAVIS_BUILD_DIR}/build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
 
