c2e0ae334efb3c93e09947f0ae1f9bb5bedafc9e
Antonio Parraga Navarro <antonio@parraga.es>
2013-03-14 16:45:42 +0100

    try Changes trying to figure out what's wrong with travis

--- build.sh
+++ build.sh
@@ -1,4 +1,6 @@
-echo $(ls -la)
+DIRFILE="dirfile.txt"
+find . -type f > $DIRFILE
+cat $DIRFILE
 export PHP_COMMAND=/usr/bin/php
 export PHING_HOME=${TRAVIS_BUILD_DIR}/build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
