b9a288ba4ed6be641c3e3141fa1eddf9a14f6098
Antonio Parraga Navarro <antonio@parraga.es>
2013-03-14 17:19:15 +0100

    Now that phing is working as expected, let replace the env variables with the TRAVIS

--- build.sh
+++ build.sh
@@ -1,4 +1,7 @@
 export PHING_HOME=${TRAVIS_BUILD_DIR}/build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
+export BUILD_NUMBER=${TRAVIS_BUILD_NUMBER}
+export CHANGELIST=${TRAVIS_COMMIT}
+export BRANCH=${TRAVIS_BRANCH}
 
 ${PHING_HOME}/bin/phing -logger phing.listener.DefaultLogger
