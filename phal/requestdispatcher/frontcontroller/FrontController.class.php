b5b9f2d79dc1771ab915e92d15c530aa80baede5
Antonio Parraga <antonio@parraga.es>
2020-06-18 11:42:23 +0200

    Removed unused function

--- phal/requestdispatcher/frontcontroller/FrontController.class.php
+++ phal/requestdispatcher/frontcontroller/FrontController.class.php
@@ -67,36 +67,6 @@ abstract class __FrontController implements __IFrontController {
         //output whatever pending (buffered) content in the response
         $response->flush();
     }
-        
-    protected function formatSizeUnits($bytes)
-    {
-        if ($bytes >= 1073741824)
-        {
-            $bytes = number_format($bytes / 1073741824, 2) . ' GB';
-        }
-        elseif ($bytes >= 1048576)
-        {
-            $bytes = number_format($bytes / 1048576, 2) . ' MB';
-        }
-        elseif ($bytes >= 1024)
-        {
-            $bytes = number_format($bytes / 1024, 2) . ' KB';
-        }
-        elseif ($bytes > 1)
-        {
-            $bytes = $bytes . ' bytes';
-        }
-        elseif ($bytes == 1)
-        {
-            $bytes = $bytes . ' byte';
-        }
-        else
-        {
-            $bytes = '0 bytes';
-        }
-
-        return $bytes;
-}
     
     public function dispatch(__IRequest &$request, __IResponse &$response) {
         //set the current request and response:
