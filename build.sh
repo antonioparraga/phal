c402a71f161beb1b38f64363b045b5db5cfcbc14
Antonio Parraga <antonio@parraga.es>
2022-03-30 12:33:23 +0200

    WIP

--- build.sh
+++ build.sh
@@ -1,8 +1,8 @@
-export PHING_HOME=${TRAVIS_BUILD_DIR}/build-libs/phing-2.4.9
+export PHING_HOME=./build-libs/phing-2.4.9
 export PHP_CLASSPATH=${PHP_CLASSPATH}:${PHING_HOME}/classes
-export VERSION=${TRAVIS_BRANCH}
-export PHP_VERSION=${TRAVIS_PHP_VERSION}
-export CHANGELIST=${TRAVIS_COMMIT}
-export BRANCH=${TRAVIS_BRANCH}
+export VERSION=${RELEASE_VERSION}
+export PHP_VERSION=${PHP_VERSION}
+export CHANGELIST=${RELEASE_VERSION}
+export BRANCH=${RELEASE_VERSION}
 
 ${PHING_HOME}/bin/phing -logger phing.listener.DefaultLogger
